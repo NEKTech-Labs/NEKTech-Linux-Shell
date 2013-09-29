ffe7a03beeca0f408ca38cd845b51352a560f007
NEK Tech Labs <pankaj.saraf@nektech.in>
2013-09-29 23:43:37 +0530

    Update input_parser.c

--- input_parser.c
+++ input_parser.c
@@ -20,6 +20,11 @@
 #include <string.h> 
 #include "nektech_shell.h"
 
+/* Taking User Input and Parsing the input to take action.
+ * String operations and intelligece for user input.
+ * Developer: Deepika Pandey
+*/ 
+
 main() 
 { 
    char cmd[MAX_LEN]; 
@@ -37,60 +42,15 @@ main()
       cmdlen--; 
       cmd[cmdlen]='\0'; 
 
-      for(i=0;i<MAX_ARG;i++) cmd_arg[i]=NULL; 
-      i=0; j=0; tag=0; 
-      while(i<cmdlen && j<MAX_ARG){ 
-         if(cmd[i]==' '){ 
-            cmd[i]='\0'; 
-            tag=0; 
-         }else{ 
-            if(tag==0) 
-               cmd_arg[j++]=cmd+i; 
-            tag=1; 
-         } 
-         i++; 
-      } 
-       
-      if(j>=MAX_ARG && i<cmdlen){ 
-         printf("TOO MANY ARGUMENTS\n"); 
-         continue; 
-      } 
-      /* cmd_arg NULL Condition. */
-      if (cmd_arg[0] == NULL )
-	  continue;       
-      /* cmd quit/exit/q: exit NEK Tech Shell */ 
-      if((strcmp(cmd_arg[0],"quit")==0) || (strcmp(cmd_arg[0],"exit")== 0) || (strcmp(cmd_arg[0],"q")== 0)) 
-         break; 
-
-      /* command change Directory */ 
-      if(strcmp(cmd_arg[0],"cd")==0){ 
-         change_dir(cmd_arg); 
-         continue; 
-      } 
-       
-      /* other cmd for fork/exec*/ 
-      run_cmd(cmd_arg); 
    }while(1); 
 } 
 
-/* cd - Change Directory */ 
+/* cd - Change Directory
+ * Shell internal Command to chanfe the present working Directory.
+ * It calls chdir(). Error conditions: EACCESS, EPERM, ENOENT.
+ * Developer: Shubhangi Maheshwari
+*/ 
 void change_dir(char *argv[]) 
 { 
-   if(argv[1]!=NULL){ 
-      if(chdir(argv[1])<0) 
-         switch(errno){ 
-         case ENOENT: 
-            printf("DIRECTORY NOT FOUND\n"); 
-            break; 
-         case ENOTDIR: 
-            printf("NOT A DIRECTORY NAME\n"); 
-            break; 
-         case EACCES: 
-            printf("YOU DO NOT HAVE RIGHT TO ACCESS\n"); 
-            break; 
-         default: 
-            printf("SOME ERROR HAPPENED IN CHDIR\n"); 
-         } 
-   } 
 
 }
