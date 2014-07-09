c80ca07f878b8248f726a3d31d77a8469e1f98f4
jitendra khasdev <jitendrakhasdev@gmail.com>
2014-07-09 16:19:45 +0530

    support for cd without args and tiled sign.

--- cmd_execution.c
+++ cmd_execution.c
@@ -4,7 +4,10 @@
  *
  * Copyright (C) NEK Tech 2013
  * Developers V1.0:  	Pallavi Gadge
+ *
  * Developers V2.0:     Jitendra Khasdev    
+ * run_cmd is renamed as nektech_run_cmd()
+ *
  * Author and Architect: Pankaj Saraf
  *
  * This program is free software; you can redistribute it and/or modify
@@ -21,84 +24,18 @@
 #include <unistd.h>
 #include <string.h>
 
-/*
- *Date : 1-july-2014
- *Basic single stage pipe support
- *Developer :jitendra khasdev
- *in this function frisrt detect pipe and then rediect output
- *inside a file and for next command it will be input
- */
 
-
-
-
-void run_cmd(char *argv[]) 
+void nektech_run_cmd(char *argv[]) 
 {
-
-char a[1000],i,flag=0,count=0,check,p,temp=0,j=0;
-char *newargv[16];
-
-
-	for(i=0;argv[i] != NULL ;i++){                /* Check weather pipe is present or not */
-            if( (check = strcmp(argv[i],"|")) == 0){
-            flag = 1;
-            }
-        }
- 
-
- 	if(flag == 0)           /*call execute_cmd function when pipe is not detected*/                         
- 	{
-             execute_cmd(argv,0);                
-	}
- 	else{
-              for(i=0,j=0;argv[i];i++){
-                  if(strcmp(argv[i],"|")!=0){
-                     newargv[j] = argv[i];
-                     j++;
-                  }
-                  else{
-                     newargv[j]='\0';
-                     j=0;    
-                     argv[i]='\0';
-                     execute_cmd(newargv,1);
-                  }
-            }
-            execute_cmd(newargv,2);
-	}
-
-}
-
-
-
-
-
-/* command Execution.*/ 
-int execute_cmd(char *argv[] , int signal)
-{
-
-int fp;
 pid_t child_pid;
 
-
-
    child_pid=fork();
-   
-   
+
    if(child_pid<0){
       printf("SOME ERROR HAPPENED IN FORK\n");
       exit(2);
    }else if(child_pid==0){
-        
-        if(signal == 1){
-        fp=open("nek.txt",O_WRONLY | O_CREAT, 0666);
-  	dup2(fp,1);
-        }
-        if(signal == 2)
-        {
-        fp=open("nek.txt",O_RDONLY);
-        dup2(fp,0);
-        }
-        if(execvp(argv[0],argv)<0)
+         if(execvp(argv[0],argv)<0)
          switch(errno){
             case ENOENT:
                printf("COMMAND OR FILENAME NOT FOUND\n");
@@ -106,15 +43,11 @@ pid_t child_pid;
             case EACCES:
                printf("YOU DO NOT HAVE RIGHT TO ACCESS\n");
                break;
-                           default:
-                                   printf("SOME ERROR HAPPENED IN EXEC\n");
+            default:
+              printf("SOME ERROR HAPPENED IN EXEC\n");
         }
-         exit(3);
-      }else
-         wait(NULL);
-
-
-
-
-return 0;
+         exit(3);                           
+      }else                                                                                                                                
+         wait(NULL);                                                                                                            
+                                                                                                                                
 }
--- input_parser.c
+++ input_parser.c
@@ -5,7 +5,12 @@
  * Copyright (C) NEK Tech 2013
  * Developers V1.0:  	Deepika Pandey
  *			Shubhangi Maheshwari
- * 
+ *
+ * Copyright (C) NEK Tech 2014
+ * Developer  V2.0:     Jitendra Khasdev
+ *                      Neha Choudhary
+ *                      Vikas Kumar
+ *
  * Author and Architect: Pankaj Saraf
  *
  * This program is free software; you can redistribute it and/or modify
@@ -37,8 +42,9 @@
 
  
 
-int search_path(char *,int);
+int nektech_search_path(char *,int);
 
+char *nk_temp_arg[1];
 
 main() 
 { 
@@ -84,11 +90,11 @@ main()
 
       /* command change Directory */
       if(strcmp(cmd_arg[0],"cd")==0){
-         change_dir(cmd_arg);
+         nektech_change_dir(cmd_arg);
          continue;
       }
       /* other cmd for fork/exec*/
-      run_cmd(cmd_arg);
+      nektech_run_cmd(cmd_arg);
    }while(1); 
 }
 
@@ -98,21 +104,19 @@ main()
  * Developer: Shubhangi Maheshwari
 */ 
 
-/*
- *cd - Change Direcotry
- *when this command is passed without any argument then
- *it will rediredt to home folder of the user
- *Developer: Jitendra Khasdev
- */
-int change_dir(char *argv[]) 
+int nektech_change_dir(char *argv[]) 
 { 
 
-int fp,c,i=0,k,j;
-static char buffer[200],temp[200];
-int End_File_Position;
-int Current_Position=1,flag=0,repeat=0,count=0;
 
    if(argv[1]!=NULL){
+      
+	/*
+	 * cd ~ command support to change the 
+ 	 * directory to user home directory. 
+ 	 */
+       if(*argv[1] == '~'){
+          argv[1]=strcat(nk_temp_arg[1],(*(argv+1)+1));         
+       }
       if(chdir(argv[1])<0)
          switch(errno){
          case ENOENT:
@@ -129,109 +133,122 @@ int Current_Position=1,flag=0,repeat=0,count=0;
          }
    } 
    else{
-         fp = open("/etc/passwd",O_RDONLY);
-         
-         if( fp == -1 ){
-               printf("\n Unable to reach /dir with error [%s]\n",strerror(errno));
-         }
-    	 else{
-              End_File_Position=lseek(fp,0L,2);  //reach the end of the file
-                        
-              lseek(fp,0L,0);     // set file pointer to start
-
-              while(i < End_File_Position){
-                   
-                for(k=0;k<200;k++) buffer[k]='\0';    //initiallzation of buffer
-
-                if(count =  read( fp, buffer, sizeof(buffer) ) ){
-
-                      for(k=0,j=0;k<200;k++,j++){
-                         temp[j]=*(buffer +k);
-                         if( *(buffer+k) == '\n' ){
-                                 if( search_path(temp,getuid()) != 0){
-                                             for(j=0;j<500;j++)
-                                               temp[j]='\0';				     
-												
-                                  j=0; 
-                                  }
-                                  else
-                                      break;
-                         }
-                      }
-                 }
-                 i=i+count;
-             }
-         } 
-     }
+        nektech_etc_passwd_search();
+        nektech_change_dir(nk_temp_arg);    
+   }
 
 return 1;    
 }     
 
 /*
- *This function finds the path of the current user's home 
- *folder 
+ *NEKTech Research Labs
+ *
+ *Function name : nektech_etc_passwd_search()
+ *Description   : This fuction is used for getting home directory path 
+ *                from /etc/passwd file.
+ *
+ *Fucntion Design : 1. open /etc/passwd file 
+ *                  2. store line till '\n' character  
+ *                  3. then call nektech_search_path(char *ptr,int userid)
+ *                  4. which give path in global variable nk_temp_arg[1]
+ *                  5. it is used in change_dir function.
+ * 
+ *Developer       : jitendra khasdev
+ *
  */
-
-int search_path(char *ptr,int userid)
+int nektech_etc_passwd_search()
 {
-int count=0,i=0,j=0,k=0,flag=0;
-char temp[100];
-char *temp_arg[1];
 
-                      	
-	 for(k=0;k<100;k++) temp[k]='\0';
-       	 while(*(ptr) != '\n')
-         {
-           if(*(ptr) == ':')
-           count++;
-           
-           if(count == 2){
-           temp[j]= *ptr;
-           j++;
+int fp,c,i=0,k,j;
+static char buffer[200],temp[200];
+int end_file_position, current_position=1,flag=0,repeat=0,count=0;
+ 
+     fp = open("/etc/passwd",O_RDONLY);
+     if( fp == -1 ){
+       printf("\n Unable to reach /dir with error [%s]\n",strerror(errno));
+     }
+     else{
+        end_file_position=lseek(fp,0L,2);  //reach the end of the file
+        lseek(fp,0L,0);     // set file pointer to start
+        while(i < end_file_position){
+         for(k=0;k<200;k++) buffer[k]='\0';    //initiallzation of buffer
+             
+            if(count = read( fp, buffer, sizeof(buffer))){
+                 for(k=0,j=0;k<200;k++,j++){
+                    temp[j]=*(buffer+k);
+                    if(*(buffer+k)=='\n'){
+                       if(nektech_search_path(temp,getuid())!= 0){
+                         for(j=0;j<200;j++)temp[j]='\0';
+                       j=0;
+                     }
+                     else
+                        break;
+                   }
+                }
            }
-       			
-           if(count == 3){
-            temp[j] = '\0';
-            j=0;  
-                 
-            for(k=0; temp[k] != '\0';k++)      // for removing : so that swapping performed
-            temp[k]=temp[k+1];
-                                 
-            temp[k]='\0';
-                            
-	      if(atoi(temp)==userid) 
-               flag=1;
-            }
-
-            if(count == 5 && flag == 1){
-             temp[j] = *ptr;
-             j++;
-            }
-
-            if(count == 6 && flag == 1  ){
-              for(k=0; temp[k] != '\0';k++)      // for removing : so that swapping performed
-              temp[k]=temp[k+1];
-                                 
-              temp[k]='\0';
-
-              flag=0;
-
-              temp_arg[1]=temp;
-                         
-              change_dir(temp_arg);
-
-              return 0;
-           }
-            
-        ptr++;
- 	}
-
-return 1;
+        i=i+count;
+        }
+     } 
+return 0;
 }
 
+/*
+ *NEKTech Research Labs
+ *
+ *
+ *function name     : nektech_search_path()
+ *Description       : This fuction is give path with respect to userid.
+ *
+ *Function Design   : 1. it is called by nektech_etc_passwd_search()
+ *                    2. now it search delimitor(":")
+ *                    3. if second delimitor then start storing string till third delimitor
+ *                    4. now compare with uid if this is found same
+ *                    5. then it start storing path from 5 to 6 delimitor.
+ *
+ *Developer         : jitendra khasdev
+ */
+int nektech_search_path(char *ptr,int userid)
+{
+  int count=0,i=0,j=0,k=0,flag=0;
+  char nk_temp[200];
+  char temp_cd[2]="cd";
 
+                        	
+  for(k=0;k<100;k++) nk_temp[k]='\0';
+  while(*(ptr) != '\n')
+  {
+    if(*(ptr) == ':')
+      count++;
+           
+    if(count == 2){
+      nk_temp[j]= *(ptr+1);
+      j++;
+    }
+       			
+    if(count == 3){
+      nk_temp[j-1] = '\0';
+      j=0;  
+    
 
+      if(atoi(nk_temp)==userid){        
+      flag=1;
+      }
+    }
 
+    if(count == 5 && flag == 1){
+     nk_temp[j] = *(ptr+1);
+      j++;
+    }
 
-
-
+    if(count == 6 && flag == 1  ){
+      
+      nk_temp[j-1]='\0';
+      flag=0;
+      j=0;
+      nk_temp_arg[1]=nk_temp;
+      return 0;
+    }
+     ptr++;
+  }
+  return 1;
+}
